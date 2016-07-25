21c4fc36c005518ceb24a6e59c8f540b68f57a87
Hervé Frezza-Buet <famille.frezzabuet@free.fr>
2016-07-25 20:25:52 +0200

    test-002 parametrable

--- tests/test-002.cpp
+++ tests/test-002.cpp
@@ -16,6 +16,11 @@
 #define LOW_DENSITY    .3
 #define LOW_MOBILITY   .3
 
+
+#define MODULE_UP
+#define MODULE_MIDDLE
+#define MODULE_DOWN
+
 int main(int argc, char* argv[]) {
   elec::Main m(argc,argv,"test-002");
 
@@ -29,29 +34,39 @@ int main(int argc, char* argv[]) {
 
   elec::World world;
 
+  auto rbag   = elec::hflip(lbag,0);
 
+#ifdef MODULE_MIDDLE
   auto lball_idf  = (world += lball);
   world                    += lbar;
-  
-  auto ulball_idf = (world += elec::translate(lball, {0,  RADIUS5}));
-  world                    += elec::translate(lbar,  {0,  RADIUS5});
-		     
-  auto blball_idf = (world += elec::translate(lball, {0, -RADIUS5}));
-  world                    += elec::translate(lbar,  {0, -RADIUS5});
+  world                    += rbag;
+  world                    += r_mat2;
+#endif
+#ifdef MODULE_UP
+  auto ulball_idf = (world += elec::translate(lball,  {0,  RADIUS5}));
+  world                    += elec::translate(lbar,   {0,  RADIUS5});
+  world                    += elec::translate(rbag,   {0,  RADIUS5});
+  world                    += elec::translate(r_mat1, {0,  RADIUS5});
+#endif		
+#ifdef MODULE_DOWN    
+  auto blball_idf = (world += elec::translate(lball,  {0, -RADIUS5}));
+  world                    += elec::translate(lbar,   {0, -RADIUS5});
+  world                    += elec::translate(rbag,   {0, -RADIUS5});
+  world                    += elec::translate(r_mat3, {0, -RADIUS5});
+#endif
 
-  auto rbag   = elec::hflip(lbag,0);
-  world += rbag;
-  world += elec::translate(rbag, {0,  RADIUS5});
-  world += elec::translate(rbag, {0, -RADIUS5});
 
-  world += elec::translate(r_mat1, {0,  RADIUS5});
-  world += r_mat2;
-  world += elec::translate(r_mat3, {0, -RADIUS5});
 
   world.build_protons();
+#ifdef MODULE_UP
   world.build_electrons(ulball_idf);
+#endif
+#ifdef MODULE_MIDDLE   
   world.build_electrons( lball_idf);
+#endif
+#ifdef MODULE_DOWN    
   world.build_electrons(blball_idf);
+#endif
 
   auto display = ccmpl::layout(16.0, 8.0, {"#"}, ccmpl::RGB(1., 1., 1.));
 
